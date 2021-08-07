eb835ab874c64ee4c18299011b04ab9f3dee46d6
Samuel_Gutierrez__ <samugutibarona@gmail.com>
2021-08-06 23:27:55 -0500

    falta que no se muevan y animacion de carga

--- animations.js
+++ animations.js
@@ -1,72 +1,59 @@
+document.addEventListener('DOMContentLoaded', main);
 
-window.addEventListener('scroll', headerAnimation);
-window.onresize = conditions;
+function main(){
+  
+  window.addEventListener('scroll', headerAnimation);
+  window.onresize = conditions;
 
-
-function headerAnimation(){
-    const scrollY = 1035;
-    if(window.scrollY > scrollY && window.innerWidth > 768){
-      var mainHeader = document.getElementById("main-header");
-      mainHeader.classList.add("white");  
-      
-    }else{
-        var mainHeader = document.getElementById("main-header");
-        mainHeader.classList.remove("white");
-      }
-      conditions();
-}
-
-window.addEventListener('load', checkboxDeclaration);
-
-function checkboxDeclaration(){
   var mainHeader = document.getElementById("main-header");
   var checkbox = document.querySelector("input[name=checkbox]");
-  
-  checkbox.addEventListener('change', function() {
-    if (this.checked) {
-      if(window.innerWidth > 768){
-        mainHeader.classList.remove("main-header-desktop");
+  const scrollY = 1035;
+
+  function headerAnimation(){
+      if(window.scrollY > scrollY && window.innerWidth > 768){
+        mainHeader.classList.add("white");  
+        
       }else{
-        mainHeader.classList.remove("main-header-mobile1");    
-      }
-      mainHeader.classList.add("main-header-mobile2");  
+          mainHeader.classList.remove("white");
+        }
+  }
     
-    } else {
-      if(window.innerWidth > 768){
-        mainHeader.classList.add("main-header-desktop");
-      
-      }else{
-        mainHeader.classList.add("main-header-mobile1");
+    checkbox.addEventListener('change', function() {
+      if (this.checked) {
+        if(window.innerWidth > 768){
+          mainHeader.classList.remove("main-header-desktop");
+        }else{
+          mainHeader.classList.remove("main-header-desktop");
+          mainHeader.classList.remove("main-header-mobile1"); 
+          mainHeader.classList.add("main-header-mobile2");     
+        }
+      }else {
+        if(window.innerWidth > 768){
+          mainHeader.classList.add("main-header-desktop");
+          mainHeader.classList.remove("main-header-mobile1"); 
+          mainHeader.classList.remove("main-header-mobile2"); 
+        
+        }else{
+          mainHeader.classList.remove("main-header-desktop");
+          mainHeader.classList.remove("main-header-mobile2");
+          mainHeader.classList.add("main-header-mobile1");
+        }
       }
-      mainHeader.classList.remove("main-header-mobile2");
+    });
+
+    function conditions(){
     
+    if(window.innerWidth < 768){
+      mainHeader.classList.add("main-header-mobile1");
+      mainHeader.classList.remove("main-header-desktop");
+
     }
-  });
-}
-
-function conditions(){
-  var mainHeader = document.getElementById("main-header");
-  if(window.innerWidth < 768){
-    mainHeader.classList.add("main-header-mobile1");
-    mainHeader.classList.remove("main-header-desktop");
-
+    
+    if(window.innerWidth > 768){
+      mainHeader.classList.remove("main-header-mobile2");
+      mainHeader.classList.remove("main-header-mobile1");
+      mainHeader.classList.add("main-header-desktop");
+      document.querySelector("input[name=checkbox]").checked = false;
+    }
   }
-  if(window.innerWidth > 768){
-    mainHeader.classList.remove("white");
-    mainHeader.classList.remove("main-header-mobile2");
-    mainHeader.classList.remove("main-header-mobile1");
-    mainHeader.classList.add("main-header-desktop");
-
-    document.querySelector("input[name=checkbox]").checked = false;
-  }
-  
-  headerAnimation();
 }
-
-
-
-
-
-
-
-
